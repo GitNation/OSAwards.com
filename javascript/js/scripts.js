6f11c6ba2be7a27acb2818dea5e8a70fd7da85e0
PixelsCommander <dradin@evolutiongaming.com>
2019-02-15 21:43:45 +0100

    Switched event to validate to input

--- javascript/js/scripts.js
+++ javascript/js/scripts.js
@@ -104,7 +104,7 @@ $(function() {
 
 	// Project submition
 
-	$('.project-submit__input').change(function(){
+	$('.project-submit__input').on("input", function(){
 		var value = $(this).val();
 		var form = $(this).closest('.project-submit');
 		var input = $(this).closest('.project-submit').find('.project-submit__input');
